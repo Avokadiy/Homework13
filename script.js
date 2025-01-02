3f268c2c057a1987b85aea31e2d9419b46c169bd
Avokadiy <braaedy1@mail.ru>
2025-01-02 19:15:02 +0500

    start

---
+++ script.js
@@ -0,0 +1,28 @@
+async function fetchUsers () {
+    try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        if (!response.ok) {
+            throw new Error('Сетевая ошибка');
+        }
+
+        const data = await response.json();
+
+        return data;
+
+    } catch (error) {
+        console.error('Ошибка', error);
+    }
+}
+
+async function main () {
+    const userList = await fetchUsers();
+    console.log(userList);
+    userList.forEach((userList) => console.log(userList.name));
+}
+
+main();
+
+// проверить корректность вывода users
+// подключение к API
+// проверить типы данных
+// интеграционное тестирование
